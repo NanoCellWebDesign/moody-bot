03298b49c239f206fc8a2128c1efd5a2c7d9a812
NanoCell Web Design <43909932+NanoCellWebDesign@users.noreply.github.com>
2020-04-01 12:56:07 +0100

    Init Insult Command

---
+++ commands/fun/instult.js
@@ -0,0 +1,47 @@
+const { MessageEmbed } = require("discord.js");
+const { stripIndents } = require("common-tags");
+const request = require("request");
+
+module.exports = {
+    name: "insult",
+    aliases: ["i",],
+    category: "bbxcentral",
+    description: "Returns all users, or one specific users info",
+    usage: "< user_name >",
+    run: async (client, message, args) => {
+        const roleColor = "#0facf3";
+
+        if (message.deletable) message.delete()
+            .then(msg => console.log(`Deleted message from ${msg.author.username}`))
+            .catch(console.error);
+
+        if (args.length < 1)
+            return message.reply('Requires a name!').then(m => m.delete()
+                .then(msg => console.log(`Deleted message from ${msg.author.username}`))
+                .catch(console.error));
+
+        console.log(`https://insult.mattbas.org/api/insult.txt?who=${args[1]}`);
+
+        request({
+            url: `https://insult.mattbas.org/api/insult.txt?who=${args[1]}`,
+            json: true
+        }, (err, response, body) => {
+            console.log(response.statusCode.toString());
+            if (response.statusCode.toString() !== '200') {
+                return message.reply('Not Found!').then(m => m.delete()
+                    .then(msg => console.log(`Deleted message from ${msg.author.username}`))
+                    .catch(console.error));
+            }
+
+            console.log(body);
+
+            const embed = new MessageEmbed()
+                .setColor(roleColor)
+                .setDescription(body)
+                .setImage(client.user.displayAvatarURL());
+
+            message.channel.send(embed);
+        });
+
+    }
+}
